5178cd3b8a7d4f4021ec21f340e295aad173a52b
Seth Thompson <thompsonrussell22@gmail.com>
2023-05-17 18:42:25 -0400

    reduced radius of spawn & recruit to 2, reduced Max Num Spawn to 2

--- src/main/java/JesterUnit.java
+++ src/main/java/JesterUnit.java
@@ -13,7 +13,7 @@
  */
 public class JesterUnit extends BartSimpsonUnit {
 
-    final static int MAX_NUM_SPAWNED = 3;
+    final static int MAX_NUM_SPAWNED = 2;
     public JesterUnit(char symbol, String name, double health, double healthModifier, double damage, double damageModifier,
                       int luck, int xCor, int yCor, int movement, int movementModifier, String teamColor, int numRecruits,
                       int numTimesSpawned, boolean distract, boolean recruit) {
@@ -29,6 +29,7 @@ public class JesterUnit extends BartSimpsonUnit {
 
     @Override
     public boolean canSpawn() {
+
         return (this.symbol == 'J' && this.getNumTimesSpawned() < MAX_NUM_SPAWNED);
     }
 
@@ -37,6 +38,49 @@ public class JesterUnit extends BartSimpsonUnit {
                 10, 5, 6, 1, 0, "Unknown", 0,
                 this.numTimesSpawned, true, true);
     }
+
+    /**
+     * JesterUnit can move anywhere on the board
+     *
+     * @param fromSquareRow    The row of the square the unit is on
+     * @param fromSquareColumn The column of the square the unit is on
+     * @param toSquareRow      The row of the square the unit is moving to
+     * @param toSquareColumn   The column of the square the unit is moving to
+     * @return True if the unit can move to the square, false otherwise
+     */
+    @Override
+    public boolean validMovePath(int fromSquareRow, int fromSquareColumn, int toSquareRow, int toSquareColumn) {
+        return true;
+    }
+
+    /**
+     * JesterUnit can spawn in a 3 space radius
+     *
+     * @param fromSquareRow    The row of the square the unit is on
+     * @param fromSquareColumn The column of the square the unit is on
+     * @param toSquareRow      The row of the square the unit is moving to
+     * @param toSquareColumn   The column of the square the unit is moving to
+     * @return True if the unit can move to the square, false otherwise
+     */
+    @Override
+    public boolean validSpawnPath(int fromSquareRow, int fromSquareColumn, int toSquareRow, int toSquareColumn) {
+        return ((Math.abs(fromSquareColumn - toSquareColumn) <= 2) && (Math.abs(toSquareRow - fromSquareRow) <= 2));
+    }
+    /**
+     * JesterUnit can recruit and spawn in the same radius of 3
+     *
+     * @param fromSquareRow    The row of the square the unit is on
+     * @param fromSquareColumn The column of the square the unit is on
+     * @param toSquareRow      The row of the square the unit is moving to
+     * @param toSquareColumn   The column of the square the unit is moving to
+     * @return True if the unit can move to the square, false otherwise
+     */
+    @Override
+    public boolean validRecruitPath(int fromSquareRow, int fromSquareColumn, int toSquareRow, int toSquareColumn) {
+        return this.validSpawnPath(fromSquareRow, fromSquareColumn, toSquareRow, toSquareColumn);
+    }
+
+
 }
 
 
